f85998bb439312e76628b7b03c122f5487b6048d
rtfee <ryan.fee625@gmail.com>
2021-02-18 13:09:57 -0500

    initial

--- example_a.tf
+++ example_a.tf
@@ -1,11 +1,11 @@
 resource "github_repository" "example_a" {
-  name        = var.repo_name
+  name        = "code_name_woohoo"
   private = true
 }
 
 resource "scalr_workspace" "example" {
-  name           = var.workspace_name
-  environment_id = var.env_id
+  name           = "code_name_woohoo"
+  environment_id = "org-ssccu6d5ch64lqg"
   vcs_provider_id = var.provider_id
   vcs_repo {
     identifier = github_repository.example_a.full_name
--- variables.tf
+++ variables.tf
@@ -2,5 +2,8 @@ variable "git_token" {
 }
 
 variable "github_org" {
-default = "rtfee"
+}
+
+variable "provider_id" {
+default = "vcs-shdp0gg2vksl5pg"
 }
